dcaf61e6e2cfdf22c1a92427ad5044be91c4ff43
SonnyTheTwin <134174783+SonnyTheTwin@users.noreply.github.com>
2023-05-28 14:41:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ScrollingMenue 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string prompt = @"INTRO SCREEN INSERT HERE ";
+            string[] Options = { "Option A", "Option B", "Option C", "Back/Exit" }; 
+            Menue mainMenue = new Menue(Options, prompt);
+            int selectedChoice = mainMenue.Run();
+        }
+    }
+}
---
+++ ScrollingMenue.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+
+namespace ScrollingMenue
+{
+    internal class Menue
+    {
+        private string[] Options; 
+        private string Prompt; 
+        private int SelectedIndex = 0;
+        public Menue(string[] options, string prompt)
+        {
+            Prompt = prompt;
+            Options = options;
+            SelectedIndex = 0;
+           
+
+        }
+        private void DisplayOptions()
+        {
+            ForegroundColor = ConsoleColor.DarkCyan;
+
+            WriteLine(Prompt); 
+
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i]; 
+                string prefix;
+                if (i == SelectedIndex) 
+                {
+                    prefix = "\x1b[36m\x1b[24m <> \x1b[4m";  
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+                else
+                {
+                    prefix = " ";
+                    ForegroundColor = ConsoleColor.DarkCyan;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+                 
+
+
+                WriteLine("" + prefix + "" + currentOption + "" + prefix); 
+            }
+            ResetColor(); 
+        }
+        public int Run()
+        {
+            ConsoleKey keypressed; 
+            do
+            {
+                Clear();
+                DisplayOptions();
+
+                ConsoleKeyInfo keyinfo = ReadKey(true); 
+                keypressed = keyinfo.Key; 
+                if (keypressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--; 
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = Options.Length - 1; 
+                    }
+                }
+                if (keypressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;                     
+                    if (SelectedIndex == Options.Length) 
+                    {
+                        SelectedIndex = 0; 
+                    }
+                }
+                if (keypressed == ConsoleKey.Enter)
+                {
+
+                }
+
+            } while (keypressed != ConsoleKey.Enter); 
+
+            return SelectedIndex;
+        }
+
+    }
+}
